50b6b4f5cbf424a884c3329ca9b8f4f40744c5ae
khrove <joshualjohnson33@gmail.com>
2023-01-16 11:15:02 -0500

    Re-study sliding window, begin divide and conquer

---
+++ patterns/divide-conquer.js
@@ -0,0 +1,11 @@
+/**
+ * @Pattern Divide and Conquer
+ * @Desc This pattern involves dividing a data set into smaller chunks and then repeating a process with a sub set of data
+ * 
+ * @Benefits This pattern can tremendously decrease time complexity
+ * 
+ * @Example Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1.
+ */
+
+
+console.log(search([1, 2, 3, 4, 5, 6], 4)); // 3
--- patterns/sliding-window.js
+++ patterns/sliding-window.js
@@ -18,24 +18,37 @@ const maxSubarraySum = (arr, target) => {
 
     // Create the first maximum sum
     for (let i = 0; i < target; i++) {
-        // This looks like 1 + 2 = 3;
+        // This looks like 2 + 6 + 9 = 17;
         maxSum += arr[i];
     }
 
     // Set the temp sum to the new max sum
-    // 0 = 3;
+    // 0 = 17;
     tempSum = maxSum;
 
     // Start the array at target since weve already added up t he beginning target of numbers
+    // Loop starts at TARGET because we dont need to know the first couple of numbers up to target
     for (let i = target; i < arr.length; i++) {
         // First run through this looks like:
-        // round 1 -- 3 - (2 - 2) + 2 == 5
+        // 17 - 2 + 2 (this is -2 because arr[i - target] is the first position in the array and arr[i] is the 3rd position, or 2)
+        // Round two looks like 17 - 6 + 1 = 12 (9 + 2 + 1)
+        // Round three looks like 12 - 9 + 8 = 11 (2 + 1 + 8)
+        // Round four looks like 11 - 2 + 5 = 14 (1 + 8 + 5)
+        // Round five looks like 14 - 1 + 6 = 19 (8 + 5 + 6)
+        // Round six looks like 19 - 8 + 3 = 14 (5 + 6 + 3)
         tempSum = tempSum - arr[i - target] + arr[i];
-        // 6 becomes maxSum because it's > 3
+        // 17 remains maxSum because 17 = 17
+        // 17 remains maxSum because 12 < 17
+        // 17 remains maxSum because 11 < 17
+        // 17 remains maxSum because 14 < 17
+        // 19 becomes maxSum because 19 > 17
+        // 19 remains maxSum because 14 < 19
         maxSum = Math.max(maxSum, tempSum);
     }
+    // Return 19
     return maxSum;
 }
 
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)); // 19
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
 console.log(maxSubarraySum([], 4)); // null
